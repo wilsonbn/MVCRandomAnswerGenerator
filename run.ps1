feaf391c45a8584de5d2f5002e5ed3004d8914bc
Brian N. Wilson <brian.wilson2@cellularsales.com>
2017-09-19 19:32:22 -0400

    adjust script

--- run.ps1
+++ run.ps1
@@ -1,6 +1,8 @@
 docker build -t mvcrandomanswers .
 
 docker images
+docker stop randomanswers
+docker rm randomanswers
 docker run -d -p 8000:8000 --name randomanswers mvcrandomanswers
-docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" randomanswers
+"http://$(docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" randomanswers):8000"
 
